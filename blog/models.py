c9c1b0ee3b6ae715adb93c45d2449c936f044cad
JooHyung <jhp21c@naver.com>
2024-03-07 16:22:55 +0900

    미리보기, 요약 추가

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from pathlib import Path
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     thumbnail_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
