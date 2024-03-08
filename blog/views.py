2c45d57ab5d45ae02cf49ba74e8279bacb5e904b
JooHyung <jhp21c@naver.com>
2024-03-08 15:44:12 +0900

    로그인한 사용자만 포스트 작성 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
 
 
@@ -25,7 +26,7 @@ class PostDetail(DetailView):
         return context
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = [
         "title",
@@ -35,6 +36,14 @@ class PostCreate(CreateView):
         "file_upload",
         "category",
     ]
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect("/blog/")
 
 
 def category_page(request, slug):
