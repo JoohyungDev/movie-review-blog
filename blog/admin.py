ba4d84cbc5edc8370e45777565c9f78a81af556b
JooHyung <jhp21c@naver.com>
2024-03-10 19:09:33 +0900

    포스트 상세페이지 댓글 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 admin.site.register(Post, MarkdownxModelAdmin)
 
@@ -17,3 +17,5 @@ class TagAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Tag, TagAdmin)
+
+admin.site.register(Comment)
