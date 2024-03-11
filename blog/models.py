a635aca97b897f2f9612cf657f2823d1eaf6c3e7
JooHyung <jhp21c@naver.com>
2024-03-11 18:41:29 +0900

    사용자 프로필사진 보여주기

--- blog/models.py
+++ blog/models.py
@@ -81,5 +81,11 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f"{self.post.get_absolute_url()}#comment-{self.pk}"
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return "https://picsum.photos/seed/{ self.author.pk }/50/50"
+
     class Meta:
         ordering = ["-created_at"]
